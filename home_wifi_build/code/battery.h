4cb077b9e82d15b22e44952f04a7a4bb8cce8490
Luigi Brancati <luigi.brancati93@gmail.com>
2022-05-06 18:04:48 +0200

    Fixed code to work better with Firebeetle board, added check for battery voltage, added hibernation

---
+++ home_wifi_build/code/battery.h
@@ -0,0 +1,25 @@
+#include <esp_adc_cal.h>
+
+#define CRITICALLY_LOW_BATTERY_VOLTAGE 3.10
+
+// Taken from https://github.com/Torxgewinde/Firebeetle-2-ESP32-E/blob/main/Firebeetle_DeepSleep.ino
+float read_battery() {
+  uint32_t value = 0;
+  int rounds = 11;
+  esp_adc_cal_characteristics_t adc_chars;
+
+  //battery voltage divided by 2 can be measured at GPIO34, which equals ADC1_CHANNEL6
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11);
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+
+  //to avoid noise, sample the pin several times and average the result
+  for(int i=1; i<=rounds; i++) {
+    value += adc1_get_raw(ADC1_CHANNEL_6);
+  }
+  value /= (uint32_t)rounds;
+
+  //due to the voltage divider (1M+1M) values must be multiplied by 2
+  //and convert mV to V
+  return esp_adc_cal_raw_to_voltage(value, &adc_chars)*2.0/1000.0;
+}
--- home_wifi_build/code/code.ino
+++ home_wifi_build/code/code.ino
@@ -1,12 +1,15 @@
 #include "wifi_server.h"
+#include "battery.h"
 #include <time.h>
 
 void setup() {
 	delay(500);
 	setCpuFrequencyMhz(80);
 	pinMode(PUMP_PIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 	pinMode(MOISTURE_READING_PIN, INPUT);
 	digitalWrite(PUMP_PIN, LOW);
+	digitalWrite(LED_BUILTIN, HIGH);
 	Serial.begin(115200);
 	while(!Serial);
 	wifi_mqtt_connect();
@@ -39,17 +42,35 @@ void setup() {
 		}
 	}
 	delay(1000);
+	// Read battery voltage
+	float batteryVoltage = read_battery();
+	Serial.println("Battery voltage: "+String(batteryVoltage));
 	if(mqttConnected){
 		// Send an off state to mean the pump/board is going to sleep and disconnect
 		esp_mqtt_client_publish(client, soilMoisture.stateTopic.c_str(), buffer, 0, 1, 1);
 		esp_mqtt_client_publish(client, pump.availabilityTopic.c_str(), "off", 3, 1, 0);
 		esp_mqtt_client_publish(client, soilMoisture.availabilityTopic.c_str(), "off", 3, 1, 0);
+		//Send battery voltage
+		createJson<float>(batteryVoltage);
+		esp_mqtt_client_publish(client, "battery/state", buffer, 0, 1, 1);
+		//disconnect
 		wifi_mqtt_disconnect();
 	}
 	// Deep sleep
-	Serial.println("Setting timer wakeup to "+String(samplingTime.value)+" seconds");
-	esp_sleep_enable_timer_wakeup((unsigned long) samplingTime.value * sToUs);
-	Serial.println("Going into sleep for "+String(samplingTime.value)+" seconds");
+	// The following lines disable all RTC features except the timer
+	esp_sleep_pd_config(ESP_PD_DOMAIN_MAX, ESP_PD_OPTION_OFF);
+	esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+	esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+	esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+	//If battery voltage is too low, set deep sleep to be infinite (i.e. hibernate)
+	if(batteryVoltage>CRITICALLY_LOW_BATTERY_VOLTAGE){
+		Serial.println("Setting timer wakeup to "+String(samplingTime.value)+" seconds");
+		esp_sleep_enable_timer_wakeup((unsigned long) samplingTime.value * sToUs);
+		Serial.println("Going into sleep for "+String(samplingTime.value)+" seconds");
+	}
+	else{
+		Serial.println("Hibernating");
+	}
 	esp_deep_sleep_start();
 }
 
